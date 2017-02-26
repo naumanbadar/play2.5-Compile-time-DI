1f14450975933d5a7d277afbcfb5c5e28e37b3c1
Nauman Badar <naumanb@kth.se>
2017-02-26 21:46:25 +0100

    akka actor receive

--- web/app/serviceActors/MyActor.scala
+++ web/app/serviceActors/MyActor.scala
@@ -2,10 +2,20 @@ package serviceActors
 
 import akka.actor.Actor
 import com.typesafe.scalalogging.LazyLogging
+import serviceActors.MyActor.Greetings
 
 /**
   * Created by nb on 16/11/02.
   */
 class MyActor extends Actor with LazyLogging {
-  override def receive: Receive = ???
+  override def receive: Receive = {
+    case Greetings =>
+      logger.debug("Greetings from Akka!")
+  }
+}
+
+object MyActor {
+
+  case object Greetings
+
 }
