9ea7e842e05ac4bc6cb9e321c411f9b546009032
Nauman Badar <naumanb@kth.se>
2017-02-28 10:15:54 +0100

    date parsed from string

--- core/src/main/scala/db/DAO.scala
+++ core/src/main/scala/db/DAO.scala
@@ -23,7 +23,7 @@ trait DAO extends LazyLogging {
   def dateToString(dateTime: LocalDateTime): String = dateTime.format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss"))
 
   //for parsing date time from string
-  def parseDateTime(str: String): LocalDateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss"))
+  def parseDateTime(str: String): LocalDateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
 
   def insertModel(model: Model): Int = {
     database.withConnection {
